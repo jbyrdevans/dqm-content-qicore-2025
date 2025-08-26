e23782555ef2fb0285a4c35e1d0114f60daede50
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2025-08-26 08:53:14 -0600

    CMS347

--- _extractMATBundle.bat
+++ _extractMATBundle.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET tooling_jar=tooling-cli-3.9.1.jar
 SET input_cache_path=%~dp0input-cache
-SET mat_bundle=bundles\mat\CMS334FHIR-v1.0.000-FHIR\CMS334FHIR-v1.0.000-FHIR.json
+SET mat_bundle=bundles\mat\CMS347FHIR-v1.0.000-FHIR\CMS347FHIR-v1.0.000-FHIR.json
 
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
